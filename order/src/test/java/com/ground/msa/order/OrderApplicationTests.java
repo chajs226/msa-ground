3410ca0609f2cc9d6540fe36bd7b142824fba158
chajs226 <chajs226@gmail.com>
2022-04-14 18:34:24 +0900

    init project
    init project

---
+++ order/src/test/java/com/ground/msa/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ground.msa.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
